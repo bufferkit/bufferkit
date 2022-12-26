a8898a8a3e02547d63a68a88f5d49cd9f343d798
Vicente Garcia <vicegax@gmail.com>
2022-12-26 14:10:08 +0100

    Update buffer

--- Sources/Buffer.swift
+++ Sources/Buffer.swift
@@ -6,7 +6,7 @@ public actor Buffer<S> where S : Bufferable {
     private var task: Task<(), Never>?
     private let url: URL
     
-    init(buffered: S, file: String) {
+    public init(buffered: S, file: String) {
         bufferable = buffered
         url = FileManager.default.urls(
             for: .documentDirectory,
@@ -15,7 +15,7 @@ public actor Buffer<S> where S : Bufferable {
                 directoryHint: .notDirectory)
     }
     
-    var buffered: S {
+    public var buffered: S {
         get async {
             if !loaded {
                 if let load: S = await Task.detached(
@@ -34,7 +34,7 @@ public actor Buffer<S> where S : Bufferable {
         }
     }
     
-    func update(buffered: S) {
+    public func update(buffered: S) {
         task?.cancel()
         loaded = true
         bufferable = buffered
